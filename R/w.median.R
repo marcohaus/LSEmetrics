347b28fbd4b7b9186a3796438c95884a5d8bbba7
marcohaus <146429691+marcohaus@users.noreply.github.com>
2025-11-25 11:32:26 +0100

    Update w.median.R

--- R/w.median.R
+++ R/w.median.R
@@ -29,7 +29,8 @@ w.median <- function(x, w, probs = seq(0, 1, 0.25), na.rm = TRUE, type = 4) {
   x <- x[ord]
   w <- w[ord]
 
-  Fx <- (cumsum(w) - min(w)) / (sum(w) - min(w))
+ # Fx <- (cumsum(w) - min(w)) / (sum(w) - min(w))
+  Fx <- (cumsum(w) ) / (sum(w) )
   keep <- !duplicated(x, fromLast = TRUE)
   x <- x[keep]
   Fx <- Fx[keep]
